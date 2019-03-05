496ec186e90045b2d4a849b9d305aabf81e797f3
zhangdadi <838780519@qq.com>
2019-03-05 11:24:16 +0800

    视频

---
+++ gsyvideo/src/main/java/com/example/g/gsyvideo/listener/BackClickListener.java
@@ -0,0 +1,11 @@
+package com.example.g.gsyvideo.listener;
+
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/11/20.
+ */
+
+public interface BackClickListener {
+        void onClick(View view);
+}
